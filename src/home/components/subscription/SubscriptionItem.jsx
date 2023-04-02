5b723c0c6381079ca2cefc8f6177a052c3b164e9
Hoang Hoa <44923253+hoanghoa12345@users.noreply.github.com>
2023-04-02 13:38:31 +0700

    Update subscription page

--- src/home/components/subscription/SubscriptionItem.jsx
+++ src/home/components/subscription/SubscriptionItem.jsx
@@ -11,71 +11,22 @@ export default function SubscriptionItem(props) {
   const itemDetails = _.slice(item.details, 0, 4);
 
   return (
-    <div key={item.id} className="group relative">
-      {
-        <div className="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 border-1">
-          <div className="grid grid-cols-2 gap-1">
-            {{
-              0: (
-                <div className="relative col-span-2">
-                  <div className="text-white text-xl absolute inset-0 bg-gray-400/80 flex justify-center items-center">
-                    Không có sản phẩm
-                  </div>
-                </div>
-              ),
-              1: itemDetails.map((item, index) => (
-                <div key={index} className="bg-gray-200 col-span-1 overflow-hidden">
-                  <img src={item.product.image} alt="" className="w-full h-1/2 object-center object-cover" />
-                </div>
-              )),
-              2: itemDetails.map((item, index) => (
-                <div key={index} className="bg-gray-200 col-span-1 row-span-1 overflow-hidden">
-                  <img src={item.product.image} alt="" className="w-full h-1/2 object-center object-cover" />
-                </div>
-              )),
-              3: itemDetails.map((item, index) => (
-                <div key={index} className="bg-gray-200 col-span-1 overflow-hidden">
-                  <img src={item.product.image} alt="" className="w-full h-full object-center object-cover" />
-                </div>
-              )),
-              4: itemDetails.map((item, index) => (
-                <div key={index} className="bg-gray-200 col-span-1 overflow-hidden">
-                  <img src={item.product.image} alt="" className="w-full h-full object-center object-cover" />
-                </div>
-              )),
-            }[itemDetails.length] || (
-              <React.Fragment>
-                <div className="bg-gray-200 col-span-1 overflow-hidden">
-                  <img src={itemDetails[0].product.image} alt={item.name} className="w-full h-full object-center object-cover" />
-                </div>
-                <div className="bg-gray-400 col-span-1 overflow-hidden">
-                  <img src={itemDetails[1].product.image} alt={item.name} className="w-full h-full object-center object-cover" />
-                </div>
-                <div className="bg-gray-400 col-span-1 overflow-hidden">
-                  <img src={itemDetails[2].product.image} alt={item.name} className="w-full h-full object-center object-cover" />
-                </div>
-                <div className="bg-gray-400 col-span-1 overflow-hidden relative">
-                  <div className="text-white bg-gray-400/80 text-xl absolute inset-0 flex justify-center items-center">
-                    + {itemDetails.length - 3}
-                  </div>
-                  <img src={itemDetails[3].product.image} alt={item.name} className="w-full h-full object-center object-cover" />
-                </div>
-              </React.Fragment>
-            )}
-          </div>
+    <div>
+      <Link to={`/subscriptions/${item.id}?name=${slugify(item.name)}&duration=${item.duration}month`}>
+        <div className="grid grid-cols-2 grid-rows-2 rounded-md overflow-hidden">
+          {itemDetails.map((item, index) => (
+            <div key={index} className="col-span-1 row-span-1">
+              <img src={`${BASE_URL}/${item.product.image}`} alt="w-16 h-16" className="object-cover" />
+            </div>
+          ))}
         </div>
-      }
-      <div className="mt-4 flex justify-between">
-        <div>
-          <h3 className="text-sm text-gray-700 font-semibold">
-            <Link to={`/subscriptions/${item.id}?name=${slugify(item.name)}&duration=${item.duration}month`}>
-              <span aria-hidden="true" className="absolute inset-0" />
-              {item.name}
-            </Link>
-          </h3>
-          <p className="mt-1 text-sm text-gray-500">{item.duration} tháng</p>
-        </div>
-        <p className="text-md font-semibold text-gray-900">{formatPrice(item.total_price)}</p>
+      </Link>
+      <div>
+        <Link to={`/subscriptions/${item.id}?name=${slugify(item.name)}&duration=${item.duration}month`} className="text-sm">
+          {item.name}
+        </Link>
+        <p className="text-gray-500 text-sm">{item.duration} tháng</p>
+        <p className="text-base text-red-500 font-semibold">{formatPrice(item.total_price)}</p>
       </div>
     </div>
   );
--- src/home/pages/EditSubscriptionPage.jsx
+++ src/home/pages/EditSubscriptionPage.jsx
@@ -48,22 +48,7 @@ const EditSubscriptionPage = () => {
     setInputQty(1);
   };
 
-  //const deleteProductMutation = useMutation((detailId) => deleteProductSubscription(detailId));
-
   const removeProductAdded = (index) => {
-    // let productToDelete = productsAdded[index];
-    // if (productToDelete.id) {
-    //   //Hanle delete product from database
-    //   deleteProductMutation.mutateAsync(productToDelete.id, {
-    //     onSuccess: () => {
-    //       productsAdded.splice(index, 1);
-    //       setProductsAdded([...productsAdded]);
-    //     },
-    //   });
-    // } else {
-    //   productsAdded.splice(index, 1);
-    //   setProductsAdded([...productsAdded]);
-    // }
     productsAdded.splice(index, 1);
     setProductsAdded([...productsAdded]);
   };
@@ -125,20 +110,20 @@ const EditSubscriptionPage = () => {
         <ToastContainer />
         <div className="flex text-sm uppercase">
           <Link to="/">eClean</Link> <ChevronRightIcon className="w-5 h-5" />
-          <Link to="/subscriptions">Gói đang ký</Link> <ChevronRightIcon className="w-5 h-5" />
+          <Link to="/subscriptions">Gói đăng ký</Link> <ChevronRightIcon className="w-5 h-5" />
           {subscription?.name}
         </div>
         <div className="my-4 grid grid-cols-1 md:grid-cols-12 gap-4">
           <div className="px-2 py-2 col-span-6 bg-white rounded-md">
             <h1 className="text-base font-semibold">Sản phẩm đã thêm:</h1>
-            <div className="flex flex-col space-y-2">
+            <div className="mt-4 flex flex-col space-y-2">
               {
                 //Loading Products Added to Subscription
                 subDetailsIsLoading &&
                   [1, 2, 3, 4].map((index) => (
                     <div
                       key={index}
-                      className="px-4 py-2 my-1 w-[350px] animate-pulse rounded-full text-gray-500 bg-gray-200 font-semibold flex"
+                      className="px-4 py-2 my-1 w-full animate-pulse rounded-md text-gray-500 bg-gray-200 font-semibold flex"
                     >
                       <span className="h-5 w-full"></span>
                     </div>
@@ -148,7 +133,7 @@ const EditSubscriptionPage = () => {
                 productsAdded.map((productAdded, i) => (
                   <div
                     key={i}
-                    className="px-4 py-2 w-full rounded-full text-gray-500 bg-gray-200 font-semibold text-sm flex cursor-pointer active:bg-gray-300 transition duration-300"
+                    className="px-4 py-2 w-full rounded-lg text-gray-500 bg-gray-200 font-semibold text-sm flex cursor-pointer active:bg-gray-300 transition duration-300"
                   >
                     <span className="">({productAdded.quantity}) </span>
 
